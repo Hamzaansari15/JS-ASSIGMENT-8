e54197d98d718c67f46a0aca7d713d7966afa39e
Hamzaansari <hamzaansaree15@gmail.com>
2022-07-05 16:06:03 -0700

    HTML AND JS FILES ADDED

---
+++ app.js
@@ -0,0 +1,14 @@
+// var number = 3.45214;
+// document.write("<h1> Number : " + number + "</h1>");
+// document.write("<h1> Round Off Value : " + Math.round(number) + "</h1>");
+// document.write("<h1> Floor value :  " + Math.floor(number) + "</h1>");
+// document.write("<h1> Ceil value : " + Math.ceil(number) + "</h1>" )
+// var num = -2.673;
+// document.write("<h1> Number : " + num + "</h1>");
+// document.write("<h1> Number : " + Math.round(num) + "</h1>");
+// document.write("<h1> Number : " + Math.floor(num) + "</h1>");
+// document.write("<h1> Number : " + Math.ceil(num) + "</h1>");
+// var num = prompt("Enter the value");
+// var absoluteValue = Math.abs(num);
+// document.write("<h1> Absolute value of " + num + " is " + absoluteValue + "</h1>");
+var randomNumber = Math.random()
